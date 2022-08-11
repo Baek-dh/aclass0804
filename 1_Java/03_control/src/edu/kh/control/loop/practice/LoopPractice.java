aa2db567fcc8524b0affd4f2e8e181c3b936331d
Baek-dh <baekdh@iei.or.kr>
2022-08-11 14:58:51 +0900

    20220811_반복문 실습문제 전체 정답

--- 1_Java/03_control/src/edu/kh/control/loop/practice/LoopPractice.java
+++ 1_Java/03_control/src/edu/kh/control/loop/practice/LoopPractice.java
@@ -4,21 +4,76 @@ import java.util.Scanner;
 
 public class LoopPractice {
 
-	public void practice1() {}
-	
-	public void practice2() {}
-	
-	public void practice3() {}
-	
-	//
-	public void practice4() {
-		/*사용자로부터 두 개의 값을 입력 받아 그 사이의 숫자를 모두 출력하세요.
-		만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
+	/* 실습문제 1
+	사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 숫자들을 모두 출력하세요.
+	단, 입력한 수는 1보다 크거나 같아야 합니다. 
+	만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
+	*/
+	public void practice1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
 
-		ex.
-		첫 번째 숫자 : 8	첫 번째 숫자 : 4	첫 번째 숫자 : 9
-		두 번째 숫자 : 4 	두 번째 숫자 : 8 	두 번째 숫자 : 0
-		4 5 6 7 8 			4 5 6 7 8 			1 이상의 숫자를 입력해주세요.*/
+		if (input >= 1) {
+			for (int i = 1; i <= input; i++) {
+				System.out.print(i + " ");
+			}
+		} else {
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		}
+	}
+	
+	
+	/* 실습문제 2
+	사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 모든 숫자를 거꾸로 출력하세요.
+	단, 입력한 수는 1보다 크거나 같아야 합니다. 
+	*/
+	public void practice2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+
+		if (input >= 1) {
+			for (int i = input; i >= 1; i--) {
+				System.out.print(i + " ");
+			}
+		} else {
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		}
+	}
+	
+	
+	/* 실습문제 3
+	1부터 사용자에게 입력 받은 수까지의 정수들의 합을 for문을 이용하여 출력하세요.
+	*/
+	public void practice3() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수를 하나 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i=1 ; i <= num ; i++) {
+			sum += i;
+			
+			if(i < num) {
+				System.out.print(i + " + ");
+			} else {
+				System.out.println(i + " = " + sum);
+			}
+		}
+	}
+	
+	/* 실습문제 4
+	 사용자로부터 두 개의 값을 입력 받아 그 사이의 숫자를 모두 출력하세요.
+	만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
+
+	ex.
+	첫 번째 숫자 : 8	첫 번째 숫자 : 4	첫 번째 숫자 : 9
+	두 번째 숫자 : 4 	두 번째 숫자 : 8 	두 번째 숫자 : 0
+	4 5 6 7 8 			4 5 6 7 8 			1 이상의 숫자를 입력해주세요.*/
+	public void practice4() {
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -76,13 +131,26 @@ public class LoopPractice {
 	
 	
 	
-	public void practice5() {}
+	/* 실습문제 5
+	 사용자로부터 입력 받은 숫자의 단을 출력하세요.
+	 */
+	public void practice5() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 : ");
+		int num = sc.nextInt();
+		
+		System.out.printf("===== %d단 =====\n", num);
+		for(int i = 1; i < 10; i++) {			
+			System.out.printf("%d * %d = %d%n", num, i, (num * i));
+		}
+	}
 	
 	
-	//
+	/* 실습문제 6
+	사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
+	단, 2~9를 사이가 아닌 수를 입력하면 “2~9 사이 숫자만 입력해주세요”를 출력하세요.*/
 	public void practice6() {
-		/*사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
-		단, 2~9를 사이가 아닌 수를 입력하면 “2~9 사이 숫자만 입력해주세요”를 출력하세요.*/
+		
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -112,23 +180,68 @@ public class LoopPractice {
 	
 	
 	
-	public void practice7() {}
-	
-	public void practice8() {}
-	
-	// 
-	public void practice9() {
-		/*
-		 * 다음과 같은 실행 예제를 구현하세요.
-
-		ex.
-		정수 입력 : 4
-		   *
-		  **
-		 ***
-		****
+	/* 실습문제7
+	다음과 같은 실행 예제를 구현하세요.
+	ex.
+	정수 입력 : 4
+	*
+	**
+	***
+	****
+	*/
+	public void practice7() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int num = sc.nextInt();
 		
-		*/
+		for(int row = 1; row <= num; row++) {
+			for(int col = 1; col <= row; col++) {				
+				System.out.print("*");
+			}
+			
+			System.out.println(); // 줄바꿈
+		}
+	}
+	
+	
+	/* 실습문제 8
+	다음과 같은 실행 예제를 구현하세요.
+	ex.
+	정수 입력 : 4
+	****
+	***
+	**
+	*
+	*/
+	public void practice8() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int num = sc.nextInt();
+		
+		for(int row = num; row > 0; row--) {
+			
+			for(int col = row; col > 0; col--) {
+				System.out.print("*");
+			}
+			
+			System.out.println(); // 줄 바꿈
+		}
+	}
+	
+	
+	/* 실습문제 9
+	다음과 같은 실행 예제를 구현하세요.
+
+	ex.
+	정수 입력 : 4
+	   *
+	  **
+	 ***
+	****
+	
+	*/ 
+	public void practice9() {
+
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -165,20 +278,20 @@ public class LoopPractice {
 	}
 	
 	
-	//
+	/* 실습문제 10
+	다음과 같은 실행 예제를 구현하세요.
+
+	ex.
+	정수 입력 : 3
+	*
+	**
+	***
+	**
+	*
+
+	*/
 	public void practice10() {
-		/*
-		다음과 같은 실행 예제를 구현하세요.
-
-		ex.
-		정수 입력 : 3
-		*
-		**
-		***
-		**
-		*
-
-		*/
+		
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -224,18 +337,18 @@ public class LoopPractice {
 		
 	}
 	
-	//
-	public void practice11() {
-		/*
-		다음과 같은 실행 예제를 구현하세요.
+	
+	/* 실습문제 11
+	다음과 같은 실행 예제를 구현하세요.
 
-		ex.
-		정수 입력 : 4
-		    *
-		   ***
-		  *****
-		 *******
-		*/
+	ex.
+	정수 입력 : 4
+	    *
+	   ***
+	  *****
+	 *******
+	*/
+	public void practice11() {
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -272,19 +385,18 @@ public class LoopPractice {
 		
 	}
 	
-	//
+	/*  실습문제 12
+	다음과 같은 실행 예제를 구현하세요.
+	ex.
+	정수 입력 : 5
+	*****
+	*   *
+	*   *
+	*   *
+	*****
+	*/
 	public void practice12() {
-		/* 
-		다음과 같은 실행 예제를 구현하세요.
-		ex.
-		정수 입력 : 5
-		*****
-		*   *
-		*   *
-		*   *
-		*****
 		
-		*/
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -306,22 +418,22 @@ public class LoopPractice {
 		}
 	}
 	
-	//
+	/* 실습문제 13
+	1부터 사용자에게 입력 받은 수까지 중에서 
+	1) 2와 3의 배수를 모두 출력하고
+	2) 2와 3의 공배수의 개수를 출력하세요.
+
+	* ‘공배수’는 둘 이상의 수의 공통인 배수라는 뜻으로 어떤 수를 해당 수들로 나눴을 때 
+	  모두 나머지가 0이 나오는 수 
+
+	ex.
+	자연수 하나를 입력하세요 : 15
+	2 3 4 6 8 9 10 12 14 15 
+	count : 2
+
+	 */
 	public void practice13() {
-		/*
-		1부터 사용자에게 입력 받은 수까지 중에서 
-		1) 2와 3의 배수를 모두 출력하고
-		2) 2와 3의 공배수의 개수를 출력하세요.
-
-		* ‘공배수’는 둘 이상의 수의 공통인 배수라는 뜻으로 어떤 수를 해당 수들로 나눴을 때 
-		  모두 나머지가 0이 나오는 수 
-	
-		ex.
-		자연수 하나를 입력하세요 : 15
-		2 3 4 6 8 9 10 12 14 15 
-		count : 2
-
-		 */
+		
 		
 		Scanner sc = new Scanner(System.in);
 		
--- 1_Java/03_control/src/edu/kh/control/loop/practice/LoopPracticeRun.java
+++ 1_Java/03_control/src/edu/kh/control/loop/practice/LoopPracticeRun.java
@@ -6,13 +6,19 @@ public class LoopPracticeRun {
 		
 		LoopPractice lp = new LoopPractice();
 
+//		lp.practice1();
+//		lp.practice2();
+//		lp.practice3();
 //		lp.practice4();
+//		lp.practice5();
 //		lp.practice6();
+//		lp.practice7();
+//		lp.practice8();
 //		lp.practice9();
 //		lp.practice10();
-		lp.practice11();
+//		lp.practice11();
 //		lp.practice12();
-//		lp.practice13();
+		lp.practice13();
 	}
 	
 }
