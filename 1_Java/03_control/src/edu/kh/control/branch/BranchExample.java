5bac70f1fd6cc2cb317c5ba0d1cc5b28ef81ceb4
Baek-dh <baekdh@iei.or.kr>
2022-08-11 16:28:23 +0900

    20220811_분기문(updown게임)

--- 1_Java/03_control/src/edu/kh/control/branch/BranchExample.java
+++ 1_Java/03_control/src/edu/kh/control/branch/BranchExample.java
@@ -66,9 +66,135 @@ public class BranchExample {
 		System.out.println("합계 : " + sum);
 		// Unreachable code : 도달할 수 없는 코드 -> 해석이 될 수 없다
 
+	}
+	
+	
+	public void ex3() {
 		
+		// "exit@" 문자열이 입력될 때 까지 문자열 누적하기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = ""; // 빈 문자열
+		
+		System.out.println("--- 문자열 입력 (종료 시 exit@ 입력) --- ");
+		while( true ) {
+			
+			String input = sc.nextLine(); // 한 줄 입력
+			
+			// 비교연산자는 보통 기본 자료형의 값 비교만 가능하다!
+			// -> String은 기본 자료형 X ,  참조형 O
+			
+			// -> 참조형은 A.equals(B)  값을 비교할 수 있다!
+			
+			// A.equals(B)      A == B
+			if(input.equals("exit@")) {
+				break;
+			}
+			
+			str += input + "\n"; // 누적하면서 줄 바꿈
+			
+		}
+
+		System.out.println(str);
 		
 	}
+	
+	
+	public void ex4() {
+		
+		// continue : 다음 반복으로 넘어감
+		
+		// 1부터 30까지 5의 배수를 제외하고 출력 (continue 사용)
+		for(int i=1 ; i<=30 ; i++) {
+			
+			if(i % 5 == 0) {
+				continue; // 다음 반복으로 이동(증감식 부분으로 이동)
+			}
+			System.out.println(i);
+		}
+	}
+	
+	
+	public void ex5() {
+		// 1 ~ 100까지 1씩 증가하며 출력하는 반복문
+		// 단, 5의 배수는 건너뛰고
+		// 증가하는 값이 40이 되었을 때 출력 후 반복을 멈춤  
+		
+		for(int i=1 ; i<=100 ; i++) {
+			
+			if(i % 5 == 0) {
+				
+				if(i == 40) {
+					System.out.println(i);
+					break;
+				}
+				
+				continue;
+			}
+			
+			System.out.println(i);
+			
+		}
+	}
+	
+	
+	public void upDownGame() {
+		
+		// 프로그램 시작 시 1 ~ 50 사이의 임의의 수 (난수)를 하나 생성하여
+		// 사용자가 몇 회 만에 맞추는지 카운트
+		
+		// 만약 틀렸을 경우 난수가 입력한 수 보다 크면 UP / 작으면 DOWN 출력
+		
+		// (임의의 수 30일 경우)
+		
+		// ex) 
+		// 1번 입력 : 10
+		// UP
+		
+		// 2번 입력 : 40
+		// Down
+		
+		// 3번 입력 : 30
+		// 정답 입니다! (총 입력 횟수 : 3회)
+		
+		
+		// 난수 생성 : Math.random()  -> Java에서 제공해주는 난수 생성 방법
+		//  0.0 <=  x  < 1.0             0.0이상 1.0미만의 난수를 생성(double 형)
+		
+		int ran = (int)(Math.random() * 50 + 1);
+		// 0.0 <=  x < 1.0  
+		// 0.0 <=  x * 50 < 50.0
+		// 1.0 <=  x * 50 +1 < 51.0
+		
+		// 1 <=  (int)(x * 50 +1) < 51  -----> 1 ~ 50 사이 난수
+		
+//		System.out.println(ran);
+		
+		Scanner sc = new Scanner(System.in);
+
+		int count = 1;
+		
+		while(true) {
+			System.out.print(count + "번 입력 : ");
+			int input = sc.nextInt();
+			
+			if(ran == input) { // 난수 == 입력값
+				System.out.printf("정답 입니다! (총 입력 횟수 : %d회) \n", count);
+				break; // while문 종료
+			} else if(ran > input) { // 난수 > 입력값
+				System.out.println("UP");
+				
+			} else { // 난수 < 입력값
+				System.out.println("DOWN");
+			}
+			
+			count++;
+		}
+	}
+	
+	
+	
 	
 	
 	
--- 1_Java/03_control/src/edu/kh/control/branch/BranchRun.java
+++ 1_Java/03_control/src/edu/kh/control/branch/BranchRun.java
@@ -6,6 +6,10 @@ public class BranchRun {
 		BranchExample branchEx = new BranchExample();
 		
 //		branchEx.ex1();
-		branchEx.ex2();
+//		branchEx.ex2();
+//		branchEx.ex3();
+//		branchEx.ex4();
+//		branchEx.ex5();
+		branchEx.upDownGame();
 	}
 }
