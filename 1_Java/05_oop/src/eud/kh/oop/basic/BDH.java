4c22787eda153e07316f6431835c5ca0fe20a684
Baek-dh <baekdh@iei.or.kr>
2022-08-16 14:46:20 +0900

    20220816_OOP(객체란? 추상화, 캡슐화)

---
+++ 1_Java/05_oop/src/eud/kh/oop/basic/BDH.java
@@ -0,0 +1,41 @@
+package eud.kh.oop.basic;
+
+// 클래스 : 객체가 되었을 때 가지고 있을 속성, 기능을 정의(작성)한 문서
+public class BDH {
+	
+	// 객체가 가지고 있는 속성
+	String name = "백동현";
+	int age = 33;
+	String birthday = "1990-01-16";
+	
+	// 캡슐화 예시
+	private String password = "1234";
+	// private : 해당 클래스 내부에서 접근 가능하다라는 뜻
+	
+	
+	// 객체가 가지고 있는 기능
+	public void study() {
+		System.out.println("자바 공부를 열심히 할 수 있음.");
+	}
+	
+	public void eat() {
+		System.out.println("밥을 맛있게 먹을 수 있음.");
+	}
+	
+	public void plus(int num1, int num2) {
+		System.out.println("합 : " + (num1 + num2));
+	}
+	
+
+	// public : 같은 프로젝트 내부 누구든, 어디서든 접근 가능
+	public void showPassword() {
+		System.out.println("비밀번호 : ***" + password + "***");
+	}
+	
+	
+	
+
+}
+
+
+
---
+++ 1_Java/05_oop/src/eud/kh/oop/basic/BasicRun.java
@@ -0,0 +1,31 @@
+package eud.kh.oop.basic;
+
+public class BasicRun {
+
+	public static void main(String[] args) {
+	
+		BDH 백동현 = new BDH(); 
+		// heap 영역에 BDH 클래스에 정의된 내용을 이용하여
+		// BDH 객체 생성(할당)
+		
+		// 객체가 가지고 있는 속성 출력
+		System.out.println("이름 : " + 백동현.name);
+		System.out.println("나이 : " + 백동현.age);
+		System.out.println("생년월일 : " + 백동현.birthday);
+		
+//		System.out.println("비밀번호 : " + 백동현.password);
+		// The field BDH.password is not visible
+		
+		// 비밀번호를 볼수 있는 기능을 호출 == 간접 접근 방법
+		백동현.showPassword(); 
+		
+		
+		
+		// 기능 수행
+		백동현.eat();
+		백동현.study();
+		백동현.plus(50, 100);
+		
+	}
+	
+}
