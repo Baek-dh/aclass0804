05a293ebfd436bc48bc60e8fa1a0c8fa63ca2c7f
Baek-dh <baekdh@iei.or.kr>
2022-08-17 17:53:01 +0900

    배열 종합 실습 문제(답안 작성 중)

--- 1_Java/04_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 1_Java/04_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -93,12 +93,62 @@ public class ArrayPractice {
 		if(flag) System.out.println("일치하는 값이 존재하지 않습니다.");
 	}
 	
-	/*실습문제 5
-	문자열을 입력 받아 문자 하나 하나를 char배열에 대입하고 
-	검색할 문자가 문자열에 몇 개 들어가 있는지, 몇 번째 인덱스에 위치하는지 인덱스를 출력하세요.
-	*/
 
-	
+	/* 실습문제 5
+	문자열을 입력 받아 문자 하나하나를 배열에 넣고 검색할 문자가 문자열에 몇 개 들어가 있는지
+	개수와 몇 번째 인덱스에 위치하는지 인덱스를 출력하세요.
+	ex.
+	문자열 : application
+	문자 : i
+	application에 i가 존재하는 위치(인덱스) : 4 8
+	i 개수 : 2
+	*/
+	public void practice5() {
+		// 1. 사용자에게 문자열과 문자 입력받기
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("문자열 : ");
+		String str = sc.nextLine();
+
+
+		// 2. 사용자가 입력한 문자열(str)을 문자 하나하나를 char배열에 넣기
+
+		// 먼저 사용자가 입력한 문자열 길이만큼의 char배열을 할당
+		char[] arr = new char[str.length()];
+
+		// 반복문을 통해 str.charAt(i) 값을 arr[i] 에 담는 과정
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = str.charAt(i);
+		}
+
+		System.out.print("문자 : ");
+		char ch = sc.nextLine().charAt(0);
+
+		// 3. 검색할 문자가 문자열에 몇개가 들어있는지와 어느 인덱스에 있는지 파악
+
+		int count = 0; 	// 검색할 문자가 문자열에 몇개가 들어있는지를 세어줄 변수
+		// 문자열에서 동일한 문자가 발생할 때마다 1씩 증가
+
+		// 반복문 수행전 문자열 출력
+		System.out.print(str + "에 " + ch + "가 존재하는 위치(인덱스) : ");
+
+		// 인덱스마다 접근하기 위해 반복문 사용
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == ch) { // 해당 인덱스 값이 검색될 문자와 같을 경우
+				System.out.print(i + " ");	// 해당 인덱스 이어서 출력
+
+				count++;	// 그리고 count 1증가
+			}
+		}
+
+		// 위에서 print()를 사용했기 때문에 개행이 되어있지 않음
+		System.out.println();	// 줄바꿈
+
+		System.out.println(ch + "개수 : " + count);
+
+
+
+	}
 	
 	
 }
