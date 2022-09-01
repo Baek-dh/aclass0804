e91c8d05b2c07ca72155b36701c528facce1c5c6
Baek-dh <baekdh@iei.or.kr>
2022-09-01 15:47:55 +0900

    20220901_HAVING절

--- 2_DB/03_GROUP BY_HAVING.sql
+++ 2_DB/03_GROUP BY_HAVING.sql
@@ -53,32 +53,56 @@ ORDER BY "인원 수"; -- 별칭 사용 O(SELECT절 해석 완료)
 
 
 
-
-
-
-
-
-
-
-
-
 -----------------------------------------------------------------------------------------------------------------------------------------
 
 -- * WHERE절 GROUP BY절을 혼합하여 사용
 --> WHERE절은 각 컬럼 값에 대한 조건 (SELECT문 해석 순서를 잘 기억하는 것이 중요!!)
+/*
+5 : SELECT
+1 : FROM
+2 : WHERE
+3 : GROUP BY
+4 : HAVING
+6 : ORDER BY
+ */
 
+-- EMPLOYEE 테이블에서 부서코드가 D5, D6인 부서의 평균 급여, 인원 수 조회
+SELECT DEPT_CODE, ROUND(AVG(SALARY)), COUNT(*)
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D5', 'D6')
+GROUP BY DEPT_CODE;
 
-
+-- EMPLOYEE 테이블에서 직급 별 2000년도 이후(2000년 포함) 입사자들의 급여 합을 조회
+-- (직급코드 오름차순)
+SELECT JOB_CODE, SUM(SALARY)
+FROM EMPLOYEE
+WHERE  
+--EXTRACT(YEAR FROM HIRE_DATE) >= 2000
+--HIRE_DATE >= TO_DATE('2000-01-01')
+--TO_NUMBER ( TO_CHAR(HIRE_DATE,'YYYY') ) >= 2000
+SUBSTR( TO_CHAR(HIRE_DATE, 'YYYY'), 1, 4) >= '2000'
+-- TO_CHAR(HIRE_DATE, 'YYYY')
+GROUP BY JOB_CODE
+ORDER BY 1;
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 
--- * 여러 컬럼을 묶어서 그룹으로 지정 가능 --> 그룹내 그룹 이 가능하다!
+-- * 여러 컬럼을 묶어서 그룹으로 지정 가능 --> 그룹 내 그룹 이 가능하다!
 -- *** GROUP BY 사용시 주의사항
 --> SELECT문에 GROUP BY절을 사용할 경우
---  SELECT절에 명시한 조회할려면 컬럼 중
+--  SELECT절에 명시한 조회하려는 컬럼 중
 --  그룹함수가 적용되지 않은 컬럼을 
 --  모두 GROUP BY절에 작성해야함.
 
+-- EMPLOYEE 테이블에서 부서별로 같은 직급인 사원의 수를 조회
+-- (부서코드 오름차순, 직급코드 내림차순)
+
+SELECT DEPT_CODE, JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE, JOB_CODE -- DEPT_CODE로 그룹을 나누고, 
+							 -- 나눠진 그룹 내에서 JOB_CODE로 또 그룹을 분류
+							 --> 세분화
+ORDER BY DEPT_CODE, JOB_CODE DESC;
 
 
 --------------------------------------------------------------------------------------------------------------------------
@@ -87,3 +111,33 @@ ORDER BY "인원 수"; -- 별칭 사용 O(SELECT절 해석 완료)
 
 -- * HAVING 절 : 그룹함수로 구해 올 그룹에 대한 조건을 설정할 때 사용
 -- HAVING 컬럼명 | 함수식 비교연산자 비교값
+/*
+5 : SELECT
+1 : FROM
+2 : WHERE
+3 : GROUP BY
+4 : HAVING
+6 : ORDER BY
+ */
+
+-- 부서별 평균 급여가 3백만원 이상인 부서를 조회(부서코드 오름차순)
+SELECT DEPT_CODE, AVG(SALARY)
+FROM EMPLOYEE
+--WHERE SALARY >= 3000000 --> 한 사람의 급여가 300백만 이상이라는 조건(요구사항 충족 X)
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) >= 3000000 --> DEPT_CODE 그룹 중 급여 평균이 3백만 이상인 그룹만 조회
+ORDER BY DEPT_CODE;
+
+
+-- EMPLOYEE 테이블에서 직급별 인원수가 5명 이하인 직급코드, 인원수 조회(직급코드 오르차순)
+SELECT JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+HAVING COUNT(*) <= 5 -- HAVING절에는 그룹 함수가 반드시 작성된다!
+ORDER BY 1;
+
+
+
+
+
+
