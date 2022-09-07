ea46c2bb6a40de203851af6f62d25f6448c38508
Baek-dh <baekdh@iei.or.kr>
2022-09-07 11:02:46 +0900

    20220907_사원관리 프로그램(PreparedStatement 사용)

--- EmployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
+++ EmployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
@@ -2,6 +2,7 @@ package edu.kh.emp.model.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,6 +22,16 @@ public class EmployeeDAO {
 	private Statement stmt;		// -> JVM이 지정한 기본값으로 초기화
 	private ResultSet rs = null;	// -> 참조형의 초기값은 null 
 									// -> 별도 초기화 안해도 된다!
+	
+	private PreparedStatement pstmt;
+	// Statement의 자식으로 향상된 기능을 제공
+	// -> ?기호 (placeholder/ 위치홀더)를 이용해서
+	//    SQL에 작성되어지는 리터럴을 동적으로 제어함
+	
+	// SQL ?기호에 추가되는 값은
+	// 숫자인 경우 '' 없이 대입
+	// 문자열인 경우 ''가 자동으로 추가되어 대입
+	
 	
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	private String url = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -43,7 +54,6 @@ public class EmployeeDAO {
 
 			// 2. JDBC 참조 변수에 객체 대입
 			// -> conn, stmt, rs에 객체 대입
-			
 			Class.forName(driver); // 오라클 jdbc 드라이버 객체 메모리 로드
 			
 			conn = DriverManager.getConnection(url, user, pw);
@@ -108,7 +118,169 @@ public class EmployeeDAO {
 		// 5. 결과 반환
 		return empList;
 	}
+
 	
+	 
+	/** 사번이 일치하는 사원 정보 조회 DAO
+	 * @param empId 
+	 * @return emp
+	 */
+	public Employee selectEmpId(int empId) {
+		
+		// 결과 저장용 변수를 선언
+		Employee emp = null; 
+		// 만약 조회 결과가 있으면 Employee 객체를 생성해서 emp에 대입 (null이 아님)
+		// 만약 조회 결과가 없으면 emp에 아무것도 대입하지 않음(null)
+		
+		try {
+			
+			Class.forName(driver); // 오라클 JDBC 드라이버 메모리 로드
+			conn = DriverManager.getConnection(url, user, pw); // 커넥션 생성해서 얻어오기
+			
+			// SQL 작성
+			String sql = 
+					  "SELECT EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE, "
+					+ "	   NVL(DEPT_TITLE, '부서없음') DEPT_TITLE, "
+					+ "	   JOB_NAME, SALARY"
+					+ " FROM EMPLOYEE"
+					+ " LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)"
+					+ " JOIN JOB USING(JOB_CODE)"
+					+ " WHERE EMP_ID = " + empId ;
+										// View에서 입력 받은 사번
+			
+			// Statement 생성
+			stmt = conn.createStatement();
+			
+			// SQL 수행 후 결과(ResultSet) 반환 받기
+			rs = stmt.executeQuery(sql);
+			
+			// ** 조회 결과가 최대 1행인 경우
+			//    불필요한 조건 검사를 줄이기 위해 if문 사용 권장 **
+			
+			if(rs.next()) { // 조회 결과가 있을 경우
+				
+//				int empId = rs.getInt("EMP_ID"); // -> 파라미터와 같은 값이므로 불필요
+				String empName = rs.getString("EMP_NAME");
+				String empNo = rs.getString("EMP_NO");
+				String email = rs.getString("EMAIL");
+				String phone = rs.getString("PHONE");
+				String departmentTitle = rs.getString("DEPT_TITLE");
+				String jobName = rs.getString("JOB_NAME");
+				int salary = rs.getInt("SALARY");
+				
+				// 조회 결과를 담은 Employee 객체 생성 후
+				// 결과 저장용 변수 emp에 대입
+				emp = new Employee(empId, empName, empNo, email,
+							phone, departmentTitle, jobName, salary);
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}finally {
+			
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(conn != null) conn.close();
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// 결과 반환
+		return emp;
+	}
+
+
+
+	/** 주민등록번호가 일치하는 사원 정보 조회 DAO
+	 * @param empNo
+	 * @return emp
+	 */
+	public Employee selectEmpNo(String empNo) {
+		
+		// 결과 저장용 변수 선언
+		Employee emp = null;
+		
+		try {
+			// Connection 생성
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			
+			// SQL 작성
+			String sql = "SELECT EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE, "
+					+ "	   NVL(DEPT_TITLE, '부서없음') DEPT_TITLE, "
+					+ "	   JOB_NAME, SALARY"
+					+ " FROM EMPLOYEE"
+					+ " LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)"
+					+ " JOIN JOB USING(JOB_CODE)"
+					+ " WHERE EMP_NO = ?";
+								// placeholder
+			
+			// Statement 객체 사용 시 순서
+			// SQL작성 -> Statement 생성 -> SQL 수행 후 결과 반환
+			
+			
+			// PreparedStatement 객체 사용 시 순서
+			// SQL 작성 
+			// -> PreparedStatement 객체 생성(?가 포함된 SQL을 매개변수로 사용)
+			// -> ?에 알맞은 값 대입
+			// -> SQL 수행 후 결과 반환
+			
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// ?에 알맞은 값 대입
+			pstmt.setString(1, empNo);
+			
+			// SQL 수행 후 결과 반환
+			rs = pstmt.executeQuery();
+			// PreparedStatement는 
+			// 객체 생성 시 이미 SQL이 담겨져 있는 상태이므로
+			// SQL 수행 시 매개변수로 전달할 필요가 없다!
+			
+			// -> 실수로 SQL을 매개변수에 추가하면
+			//  ?에 작성 추가했던 값이 모두 사라져 수행 시 오류 발생 
+			
+			if(rs.next()) {
+				int empId = rs.getInt("EMP_ID"); 
+				String empName = rs.getString("EMP_NAME");
+				
+//				String empNo = rs.getString("EMP_NO"); // -> 파라미터와 같은 값이므로 불필요
+				
+				String email = rs.getString("EMAIL");
+				String phone = rs.getString("PHONE");
+				String departmentTitle = rs.getString("DEPT_TITLE");
+				String jobName = rs.getString("JOB_NAME");
+				int salary = rs.getInt("SALARY");
+				
+				// 조회 결과를 담은 Employee 객체 생성 후
+				// 결과 저장용 변수 emp에 대입
+				emp = new Employee(empId, empName, empNo, email,
+							phone, departmentTitle, jobName, salary);
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// 결과 반환
+		return emp;
+	}
 	
 	
 	
--- EmployeeManagement/src/edu/kh/emp/view/EmployeeView.java
+++ EmployeeManagement/src/edu/kh/emp/view/EmployeeView.java
@@ -17,7 +17,6 @@ public class EmployeeView {
 	
 	
 	
-	
 	// 메인 메뉴
 	public void displayMenu() {
 		
@@ -32,9 +31,12 @@ public class EmployeeView {
 				System.out.println("3. 사번이 일치하는 사원 정보 조회");
 				System.out.println("4. 사번이 일치하는 사원 정보 수정");
 				System.out.println("5. 사번이 일치하는 사원 정보 삭제");
-				System.out.println("6. 입력 받은 부서와 일치 모든 사원 정보 조회");
+				System.out.println("6. 입력 받은 부서와 일치하는 모든 사원 정보 조회");
 				System.out.println("7. 입력 받은 급여 이상을 받는 모든 사원 정보 조회");
 				System.out.println("8. 부서별 급여 합 전체 조회");
+				
+				System.out.println("9. 주민등록번호가 일치하는 사원 정보 조회");
+				
 				System.out.println("0. 프로그램 종료");
 				
 				System.out.print("메뉴 선택 >> ");
@@ -46,12 +48,15 @@ public class EmployeeView {
 				switch(input) {
 				case 1:     break;
 				case 2:  selectAll();  break;
-				case 3:     break;
+				case 3:  selectEmpId();   break;
 				case 4:     break;
 				case 5:     break;
 				case 6:     break;
 				case 7:     break;
 				case 8:     break;
+				
+				case 9:  selectEmpNo();   break;
+				
 				case 0:  System.out.println("프로그램을 종료합니다...");   break;
 				default: System.out.println("메뉴에 존재하는 번호만 입력하세요.");
 				}
@@ -104,6 +109,68 @@ public class EmployeeView {
 	}
 	
 	
+	/**
+	 * 사번이 일치하는 사원 정보 조회
+	 */
+	public void selectEmpId() {
+		System.out.println("<사번이 일치하는 사원 정보 조회>");
+		
+		// 사번 입력 받기
+		int empId = inputEmpId();
+		
+		// 입력 받은 사번을 DAO의 selectEmpId() 메서드로 전달하여
+		// 조회된 사원 정보 반환 받기
+		Employee emp = dao.selectEmpId(empId); 
+		
+		printOne(emp); // 조회 결과 출력
+		
+	}
+	
+	
+	/** 사번을 입력 받아 반환하는 메서드
+	 * @return empId
+	 */
+	public int inputEmpId() {
+		System.out.print("사번 입력 : ");
+		int empId = sc.nextInt();
+		sc.nextLine(); // 버퍼에 남은 개행문자 제거
+		return empId;
+	}
+	
+	
+	/** 사원 1명 정보 출력
+	 * @param emp
+	 */
+	public void printOne(Employee emp) {
+		
+		if(emp == null) {
+			System.out.println("조회된 사원 정보가 없습니다.");
+			
+		} else {
+			System.out.println("사번 |   이름  | 주민 등록 번호 |        이메일        |   전화 번호   | 부서 | 직책 | 급여" );
+			System.out.println("------------------------------------------------------------------------------------------------");
+			
+			System.out.printf(" %2d  | %4s | %s | %20s | %s | %s | %s | %d\n",
+					emp.getEmpId(), emp.getEmpName(), emp.getEmpNo(), emp.getEmail(), 
+					emp.getPhone(), emp.getDepartmentTitle(), emp.getJobName(), emp.getSalary());
+		}
+		
+	}
+	
+	
+	/**
+	 * 주민등록번호가 일치하는 사원 정보 조회
+	 */
+	public void selectEmpNo() {
+		System.out.println("<주민등록번호가 일치하는 사원 정보 조회>");
+		
+		System.out.print("주민등록번호 입력 : ");
+		String empNo = sc.next();
+		
+		Employee emp = dao.selectEmpNo(empNo);
+		
+		printOne(emp);
+	}
 	
 	
 	
